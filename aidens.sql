efc606967f7d856d8f79bb0e473f2b9054d80db4
aidenisaman <westac@sunypoly.edu>
2024-09-20 14:50:22 -0400

    Updated queries and added #10

--- aidens.sql
+++ aidens.sql
@@ -1,11 +1,24 @@
+-- 10. Show how many employees in the IT department are paid more than the manager of the Shipping department
+SELECT COUNT(*) AS employee_count
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE LOWER(d.department_name) = 'it'
+  AND e.salary > (
+    SELECT MAX(m.salary)  -- Use MAX to ensure a single value is returned
+    FROM employees m
+    JOIN departments d2 ON m.department_id = d2.department_id
+    JOIN jobs j ON m.job_id = j.job_id
+    WHERE LOWER(d2.department_name) = 'shipping'
+      AND LOWER(j.job_title) LIKE '%manager%'
+  );
+
+
 -- 11. Using "set-theory SQL", show the employee id, first name and last name of each employee who is not managed by Nancy Greenberg
-SELECT employee_id, first_name, last_name
-FROM employees
-MINUS
 SELECT e.employee_id, e.first_name, e.last_name
 FROM employees e
-JOIN employees m ON e.manager_id = m.employee_id
-WHERE m.first_name = 'Nancy' AND m.last_name = 'Greenberg';
+LEFT JOIN employees m ON e.manager_id = m.employee_id
+WHERE m.first_name IS NULL OR NOT (m.first_name = 'Nancy' AND m.last_name = 'Greenberg');
+
 
 -- 12. Using "set-theory SQL", show the employee id and last names of all employees paid more than 9000 or who work in Germany
 (SELECT e.employee_id, e.last_name
@@ -18,6 +31,7 @@ JOIN departments d ON e.department_id = d.department_id
 JOIN locations l ON d.location_id = l.location_id
 JOIN countries c ON l.country_id = c.country_id
 WHERE c.country_name = 'Germany');
+
 
 -- 13. Using "set-theory SQL", show the countries in region 4 whose names begin with the letter 'Z'
 SELECT country_name
